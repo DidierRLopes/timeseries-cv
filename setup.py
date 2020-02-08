8d135f347c9cec325d62bcbad16fecd2d819ee1e
didier <dro.lopes@campus.fct.unl.pt>
2020-02-08 18:16:38 +0000

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 from setuptools import setup
 
 setup(
-    # Needed to silence warnings
     name='TimeSeriesCrossValidation',
-    url='',
-    author='Didier Lopes',
+    url='https://github.com/DidierRLopes',
+    author='Didier Rodrigues Lopes',
     author_email='dro.lopes@campus.fct.unl.pt',
-    # Needed to actually package something
-    packages=['TimeSeriesCrossValidation.splitTrain'],
-    # Needed for dependencies
+    packages=['TimeSeriesCrossValidation'],
     install_requires=['numpy'],
-    # *strongly* suggested for sharing
     version='0.1',
-    license='MIT',
-    description='An example of a python package from pre-existing code',
-    # We will also need a readme eventually (there will be a warning)
+    license='LICENSE.txt',
+    description='Implementation of Cross-Validation techniques (Forward Chaining, K-Fold, Group K-Fold) to Time-Series',
     long_description=open('README.rst').read(),
-    # if there are any scripts
-    scripts=['scripts/testSplit.py'],
+    scripts=['scripts/testSplit.py']
 )
