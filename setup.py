d1edd429f1adf9f2e9ab6f078ce455455ff81f31
didier <dro.lopes@campus.fct.unl.pt>
2020-02-08 17:45:25 +0000

    add splitTrain package

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
     author='Didier Lopes',
     author_email='dro.lopes@campus.fct.unl.pt',
     # Needed to actually package something
-    #packages=['TimeSeriesCrossValidation'],
-    packages=find_packages(),
+    packages=['TimeSeriesCrossValidation.splitTrain'],
     # Needed for dependencies
     install_requires=['numpy'],
     # *strongly* suggested for sharing
