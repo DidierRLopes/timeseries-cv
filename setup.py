b425bbf7d3a642b50a819f1080795711bc694016
didier <dro.lopes@campus.fct.unl.pt>
2020-03-10 00:10:49 +0000

    update version of module

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='dro.lopes@campus.fct.unl.pt',
     packages=['TimeSeriesCrossValidation'],
     install_requires=['numpy'],
-    version='0.1',
+    version='0.3',
     license='LICENSE.txt',
     description='Implementation of Cross-Validation techniques (Forward Chaining, K-Fold, Group K-Fold) to Time-Series',
     long_description=open('README.md').read(),
